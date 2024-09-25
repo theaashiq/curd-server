d293f62e9e847d0d1e5f503ac57cd6e42df73000
theaashiq <mdaashiqin2000@gmail.com>
2024-09-25 20:07:34 +0530

    updates

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const { logger } = require(path.join(__dirname, 'middleware', 'logEvents'))
-const corsOptions = require(path.join(__dirname, 'config', 'corsOptions'))
-const errorHandler = require(path.join(__dirname, 'middleware', 'errorHandling'))
-const connectDB = require('./config/dbConfig')
+
+const { logger } = require('./middleware/logEvents');
+const corsOptions = require('./config/corsOptions');
+const errorHandler = require('./middleware/errorHandling');
+const connectDB = require('./config/dbConfig');
+// const { logger } = require(path.join(__dirname, 'middleware', 'logEvents'))
+// const corsOptions = require(path.join(__dirname, 'config', 'corsOptions'))
+// const errorHandler = require(path.join(__dirname, 'middleware', 'errorHandling'))
+// const connectDB = require('./config/dbConfig')
 
 const PORT = process.env.PORT || 5600
 
@@ -17,7 +22,8 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.use('/', require(path.join(__dirname, 'routes', 'mainRoutes')))
+// app.use('/', require(path.join(__dirname, 'routes', 'mainRoutes')))
+app.use('/', require('./routes/mainRoutes'))
 
 app.all('*', (req, res) => {
     res.status(404);
