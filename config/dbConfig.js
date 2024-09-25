9daa2d87afcacda534f6f481bc831cc17fb07786
theaashiq <mdaashiqin2000@gmail.com>
2024-09-25 20:19:26 +0530

    updates

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
+const MONGO_URI = 'mongodb+srv://theaashiq:Livpop14131@cluster0.8qk1l.mongodb.net/curdDB?retryWrites=true&w=majority'
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(MONGO_URI)
         console.log('MongoDB connected Successfully')
     } catch(err)  {
         console.error(err)
