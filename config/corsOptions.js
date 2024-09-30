0f5b3755cdff3bde84a903cdfd32e1d601f45d16
theaashiq <mdaashiqin2000@gmail.com>
2024-10-01 05:21:47 +0530

    updates

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,7 +3,8 @@ const whiteList = [
     'http://127.0.0.1:5400',
     'http://localhost:5600',
     'http://localhost:5173',
-    'https://curd-view-main.vercel.app/'
+    'https://curd-view-main.vercel.app',
+    'https://curd-view-main-n9m8z5pqc-theaashiqs-projects.vercel.app'
 ]
 
 const corsOptions = {
