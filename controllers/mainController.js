61a459982b4d97c7570577c9a8ad8692c35094a5
theaashiq <mdaashiqin2000@gmail.com>
2024-10-06 02:40:05 +0530

    updates

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -20,7 +20,17 @@ const fetchMainData = async ( req, res ) => {
             adult: await Main.countDocuments({age: {$gte: 19, $lte: 45}}),
             senior: await Main.countDocuments({age: {$gt: 45}}),
         }
-        const customersData = await Main.find().sort({ createdAt: -1 })
+        let customersData;
+        switch (req.body.sortBy) {
+            case 'name' :
+                customersData = await Main.find().sort({ firstName: 1 })    
+            break
+            case 'date' :
+                customersData = await Main.find().sort({ createdAt: -1 })
+            break
+            default:
+                customersData = await Main.find();
+        }
         res.status(200).json({
             dashboard : dashboardData,
             customers: customersData
